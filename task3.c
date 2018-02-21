39913ad16afecc4cced350aa76f86063d33713ac
Michael Küchler <33421646+MrMichael3@users.noreply.github.com>
2018-02-22 00:06:11 +0100

    input is done, isPrime function empty

--- task3.c
+++ task3.c
@@ -1,1 +1,15 @@
 //Michael Küchler, 16-924-318
+#include <stdio.h>
+int isPrime(int a){
+	
+	return 0;
+}
+int main(){
+	int A[100];
+	int i;
+	while(scanf("%i",A[i]==1)){
+		i++;
+	}
+	
+	return 0;
+}
