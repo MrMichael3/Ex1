32de53210ce38fe6415a03ac6036e6c96ce965ed
Michael Küchler <33421646+MrMichael3@users.noreply.github.com>
2018-02-21 23:18:13 +0100

    added loop to count sums of odd and even numbers

--- task2.c
+++ task2.c
@@ -2,8 +2,20 @@
 
 int main(){
 	int A[100];
+	int i = 0;
 	while (scanf("%i",&A[i])==1){
 		i++;
 	}
+	int evenSum;
+	int oddSum;
+	for (x = 0;x < i;x++){
+		if (A[x] % 2 == 0){
+			evenSum += A[x];
+		}
+		else{
+			oddSum += A[x];
+		}
+	}
+	
 	return 0;
 }
