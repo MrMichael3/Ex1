9cbf82fc23f1b76c9e56fe1cc506af9b26d11dde
Michael Küchler <33421646+MrMichael3@users.noreply.github.com>
2018-02-21 22:46:39 +0100

    Added task 6, crash when running!

---
+++ task6.c
@@ -0,0 +1,18 @@
+//Michael Küchler, 16-924-318
+
+int power(int base, int exponent){
+	
+	return 0;
+}
+int main(){
+	int base;
+	int exponent;
+	int result;
+	printf("Enter the base: ");
+	scanf("%d",base);
+	printf("Enter the exponent: ");
+	scanf("%d",exponent);
+	result = power(base,exponent);
+	printf("Result: %d",result);
+	return 0;
+}
