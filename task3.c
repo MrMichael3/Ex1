9efad612a8213b60dee7a78551de4992e1b52990
Michael Küchler <33421646+MrMichael3@users.noreply.github.com>
2018-02-22 08:55:41 +0100

    added isPrime function

--- task3.c
+++ task3.c
@@ -1,6 +1,13 @@
 //Michael Küchler, 16-924-318
 #include <stdio.h>
 int isPrime(int a){
+	int x;
+	for(x=2;x < a;x++){
+		if (a%x == 0){
+			return 0;
+		}
+	return 1;
+	}
 	
 	return 0;
 }
@@ -10,6 +17,7 @@ int main(){
 	while(scanf("%i",A[i]==1)){
 		i++;
 	}
+	
 	
 	return 0;
 }
