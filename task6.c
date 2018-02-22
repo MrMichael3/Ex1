e28cb72951fc2bb40d596514198708207d81e8c4
Michael Küchler <33421646+MrMichael3@users.noreply.github.com>
2018-02-22 10:55:42 +0100

    fixed task6

--- task6.c
+++ task6.c
@@ -1,17 +1,19 @@
 //Michael Küchler, 16-924-318
 
 int power(int base, int exponent){
-	
-	return 0;
+	if (exponent == 1){
+		return base;
+	}
+	return base * power(base, exponent-1);
 }
 int main(){
 	int base;
 	int exponent;
 	int result;
 	printf("Enter the base: ");
-	scanf("%d",base);
+	scanf("%d",&base);
 	printf("Enter the exponent: ");
-	scanf("%d",exponent);
+	scanf("%d",&exponent);
 	result = power(base,exponent);
 	printf("Result: %d",result);
 	return 0;
