e1712a8d837b319e16e31536c60b1054de23db78
Michael Küchler <33421646+MrMichael3@users.noreply.github.com>
2018-03-01 09:03:24 +0100

    finished task 7

--- task7.c
+++ task7.c
@@ -1,1 +1,23 @@
 //Michael Küchler, 16-924-318
+#include <stdio.h>
+
+void sequence(int n){
+	printf("%d ",n);
+	if(n!=1){
+		//even
+		if(n%2==0){
+			sequence(n/2);
+		}
+		//odd
+		else{
+			sequence(3*n+1);
+		}
+	}
+}
+int main(){
+	int firstNumber;
+	printf("Enter the first sequence number: ");
+	scanf("%d",&firstNumber);
+	sequence(firstNumber);
+	return 0;
+}
